3382a2c3f2b30c03410c7b781079ac4f80bbbbf5
JamesBond <introbond.dev@gmail.com>
2023-04-11 20:53:05 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SpaceBackground from "./components/SpaceBackground";
+
+const App = () => {
+  return (
+    <div className="app">
+      <SpaceBackground />
+    </div>
+  );
+};
+
+export default App;
